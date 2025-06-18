2f50caf50659ba88d13302534cad4bf8a574e048
Victorfncc <lindembergvictor2018@gmail.com>
2025-06-18 13:30:26 -0300

    Update app.py

--- app.py
+++ app.py
@@ -71,5 +71,8 @@ def command():
     output = send_command(cmd)
     return jsonify({"output": output})
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
